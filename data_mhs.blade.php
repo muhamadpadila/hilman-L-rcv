9ad4a2a0bca0b4e89096a32eb2d43e0afc6b83ba
muhamadpadila <144421993+muhamadpadila@users.noreply.github.com>
2024-02-13 14:05:43 +0700

    Create data_mhs.blade.php

---
+++ data_mhs.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasantri</title>
+</head>
+<body align="center">
+    <h1>Data Mahasantri Petik II Jombang</h1>
+    <table border="1" cellpadding="10" align="center">
+        <thead>
+            <tr>
+                <th align="center">Nomor</th>
+                <th align="center">Nama</th>
+                <th align="center">Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td align="center">1</td>
+                <td>{{ $mhs1 }}</td>
+                <td>{{ $asal1 }}</td>
+            </tr>
+            <tr>
+                <td align="center">2</td>
+                <td>{{ $mhs2 }}</td>
+                <td>{{ $asal2 }}</td>
+            </tr>
+            <tr>
+                <td align="center">3</td>
+                <td>{{ $mhs3 }}</td>
+                <td>{{ $asal3 }}</td>
+            </tr>
+            <tr>
+                <td align="center">4</td>
+                <td>{{ $mhs4 }}</td>
+                <td>{{ $asal4 }}</td>
+            </tr>
+            <tr>
+                <td align="center">5</td>
+                <td>{{ $mhs5 }}</td>
+                <td>{{ $asal5 }}</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
